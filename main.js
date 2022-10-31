4a10841348701698bb1b69ed9c81842bbec15fa1
Jonas-Edu <jonasrodrigues7@yahoo.com.br>
2022-10-31 14:58:01 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,61 @@
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const menu = document.querySelector('nav .container ul');
+
+menuBtn.addEventListener('click', () =>{
+    menu.style.display = 'block';
+    menuBtn.style.display = 'none';
+    closeBtn.style.display = 'inline-block';
+})
+
+closeBtn.addEventListener('click', () =>{
+    menu.style.display = 'none';
+    menuBtn.style.display = 'inline-block';
+    closeBtn.style.display = 'none';
+})
+
+
+const navItems = menu.querySelectorAll('li');
+
+const changeActiveItem = () => {
+    navItems.forEach(item => {
+    const link = item.querySelector('a');
+    link.classList.remove('ativo');
+    })
+}
+
+navItems.forEach(item => {
+    const link = item.querySelector('a');
+    link.addEventListener('click', () => {
+        changeActiveItem();
+        link.classList.add('ativo');
+    })
+})
+
+// mais info
+
+const readMoreBtn = document.querySelector('.read-more');
+const readMoreContent = document.querySelector('.read-more-content');
+
+readMoreBtn.addEventListener('click', () => {
+    readMoreContent.classList.toggle('show-content');
+    if(readMoreContent.classList.contains('show-content')){
+        readMoreBtn.textContent = "Menos Info";
+    } else{
+        readMoreBtn.textContent = "Mais Info";
+    }
+})
+
+// apresenta habilidades items
+
+const skillItems = document.querySelectorAll('section.habilidades .habilidade');
+
+skillItems.forEach(skill =>{
+    skill.querySelector('.head').addEventListener('click', ()=>{
+        skill.querySelector('.items').classList.toggle('show-items');
+    })
+})
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('show-box-shadow', window.scrollY > 0)
+})
